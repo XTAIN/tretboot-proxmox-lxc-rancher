7daea7fc7f5ab84423ab100da5b5e693a62ab6db
Maximilian Ruta <mruta@xtain.net>
2024-06-01 12:35:02 +0200

    add private key

--- install.sh
+++ install.sh
@@ -138,6 +138,17 @@ EOF
   ((i++))
 done
 
+configmap_kubemox=""
+if [ "${proxmox_api}" == "1" ]; then
+  configmap_kubemox="$(cat - <<EOF
+      kubemox:
+          repo: "${repository}"
+          branch: "main"
+          path: "kubemox"
+EOF
+)"
+fi
+
 configmap_git_ssh=""
 if [ "${fleet_ssh_key}" ]; then
   temp_private_key=$(mktemp)
@@ -222,6 +233,7 @@ data:
     repository:
       url: "$repository"
     extraRepositories:
+$configmap_kubemox
 $configmap_extra_repositories
 $configmap_proxmox_api
 $configmap_git_ssh
